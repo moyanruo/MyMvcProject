01a2bbd399df9196ce0951e6f5e26a97bb431485
moyanruo <8427368612qq.com>
2018-01-20 19:04:37 +0800

    first commit

---
+++ WebPage/WebPage/Areas/ComManage/ComManageAreaRegistration.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace WebPage.Areas.ComManage
+{
+    public class ComManageAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "ComManage";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                //"ComManage_default",
+                //"ComManage/{controller}/{action}/{id}",
+                "ComManage_default",
+                "Com/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                new string[] { "WebPage.Areas.ComManage.Controllers" }
+            );
+        }
+    }
+}
